44ebbd99103bd273875a3543142872bd463d5051
Alexius Wadell <awadell@gmail.com>
2021-07-03 14:18:35 -0400

    Plot Memory Efficency

--- dev/usage_stats.py
+++ dev/usage_stats.py
@@ -43,12 +43,21 @@ def __parse_duration(str):
         days=days, hours=hours, minutes=minutes, seconds=seconds
     ).total_seconds()
 
-RE_TRES = re,compile(r"(.*?)=([^,]*),?")
-def __parse_tres(str):
-    tres = {}
-    for m in re.findall(str):
-        tres[m.group(1)] = m.group(2)
-    return tres
+
+USAGE_REGEXP = re.compile(r"([\d\.]+)(\w+)")
+
+
+def parse_bytes(str):
+    """ Convert du human readable usage into bytes 1K -> 1024 """
+    m = USAGE_REGEXP.match(str)
+    if m:
+        number = float(m.group(1))
+        unit = m.group(2)
+        unit_idx = ["K", "M", "G", "T", "P", "E", "Z", "Y"].index(unit)
+        return float(number) * 1024 ** (unit_idx + 1)
+    else:
+        return float(str)
+
 
 JOB_ID_SPLIT = re.compile(r"[\.+_]")
 
@@ -70,6 +79,11 @@ def sacct():
         ("Timelimit", __parse_duration),
         ("TotalCPU", __parse_duration),
         ("CPUTime", __parse_duration),
+        ("MaxRSS", lambda x: parse_bytes(x) if x else 0),
+        ("ReqMem", lambda x: {"mem": parse_bytes(x[0:-1]), "type": x[-1]}),
+        ("NCPUS", int),
+        ("NNodes", int),
+        ("NTasks", lambda x: int(x) if x else None),
     ]
     cmd = [
         "sacct",
@@ -84,7 +98,9 @@ def sacct():
     ) """
     with open("sacct.out", "r") as io:
         records = __sacct_record(fields, io)
-        return pd.DataFrame.from_records(records)
+        df = pd.DataFrame.from_records(records)
+
+    return __parse_reqmem(df)
 
 
 def __sacct_record(keys, stream):
@@ -113,33 +129,66 @@ def plot_wait_times(ax, df):
     ax.set_xscale("linear")
 
 
-def plot_job_eff(ax, df) -> pd.DataFrame:
-    def cpu_efficency(job_steps):
-        print(job_steps)
-        cpu_alloc = job_steps.groupby("JobIdRaw").sum()
-        cpu_used = job_steps.TotalCPU.sum()
-        if cpu_alloc.seconds > 0:
-            return cpu_used / cpu_alloc
-        return None
+# Convert ReqMem to bytes
+def __parse_reqmem(df):
+    def tf(job):
+        mem_bytes = job["ReqMem"]["mem"]
+        mem_type = job["ReqMem"]["type"]
+        if mem_type == "n":
+            return mem_bytes * job["NNodes"]
+        elif mem_type == "c":
+            return mem_bytes * job["NCPUS"]
+        else:
+            LOG.warn("Got ReqMem type of %s for job %d", mem_type, job["JobId"])
+            return mem_bytes
 
+    df["ReqMemOut"] = df.apply(tf, axis=1)
+    print(df)
+    df["ReqMem"] = df["ReqMemOut"]
+    return df
+
+
+def plot_job_eff(ax, df) -> pd.DataFrame:
+    # Extract Allocated Memory from ReqTRES and reduce to required columns
     job_eff = df.groupby("JobId").filter(lambda x: (x.State == "COMPLETED").all())
-    job_eff = job_eff[["User", "JobId", "JobIdRaw", "CPUTime", "TotalCPU"]]
-    job_eff.to_csv("job_eff.csv")
-    job_eff = job_eff.groupby(["JobId", "JobIdRaw"]).agg(
-        {"CPUTime": "max", "TotalCPU": "max"}
-    )
-    job_eff = job_eff.groupby("JobId").agg({"CPUTime": "sum", "TotalCPU": "sum"})
     job_eff = job_eff[job_eff.CPUTime > 0]
+    job_eff["MaxRSS"] = job_eff["MaxRSS"] * job_eff["NTasks"]
+    job_eff = job_eff[
+        ["User", "JobId", "JobIdRaw", "CPUTime", "TotalCPU", "ReqMem", "MaxRSS"]
+    ]
+
+    job_eff = job_eff.groupby(["JobId", "JobIdRaw"]).agg(
+        {"CPUTime": "max", "TotalCPU": "max", "ReqMem": "max", "MaxRSS": "sum"}
+    )
+    job_eff = job_eff.groupby("JobId").agg(
+        {"CPUTime": "sum", "TotalCPU": "sum", "ReqMem": "sum", "MaxRSS": "sum"}
+    )
+
+    # Compute Efficiencies
     job_eff["cpu_eff"] = 100 * (job_eff.TotalCPU / job_eff.CPUTime)
+    job_eff["mem_eff"] = 100 * (job_eff.MaxRSS*job_eff.TotalCPU) / (job_eff.ReqMem *job_eff.TotalCPU)
+
+    # Plot CPU Efficiencies
     job_eff["cpu_eff"].hist(
-        ax=ax,
+        ax=ax[0],
         bins=25,
         range=(0, 100),
         grid=False,
         log=True,
     )
-    ax.set_xlabel("CPU Efficiency [%]")
-    ax.set_ylabel("Job Count")
+    ax[0].set_xlabel("CPU Efficiency [%]")
+    ax[0].set_ylabel("Job Count")
+
+    # Plot Memory Efficiencies
+    job_eff["mem_eff"].hist(
+        ax=ax[1],
+        bins=25,
+        range=(0, 100),
+        grid=False,
+        log=True,
+    )
+    ax[1].set_xlabel("Memory Efficiency [%]")
+    ax[1].set_ylabel("Job Count")
 
     # Return for further processing
     return job_eff
@@ -149,10 +198,10 @@ if __name__ == "__main__":
     df = sacct()
     df.to_csv("sacct.csv")
 
-    fig, axes = plt.subplots(nrows=1, ncols=2)
+    fig, axes = plt.subplots(nrows=2, ncols=2)
 
     # Plot Wait Times
-    plot_wait_times(axes[0], df)
+    plot_wait_times(axes[0, 0], df)
 
     # Compute CPU Efficiency
     job_eff = plot_job_eff(axes[1], df)
@@ -170,13 +219,17 @@ if __name__ == "__main__":
     )
     user_job_eff = job_eff.join(job_users, on="JobId")
     user_job_eff = user_job_eff.groupby("User").agg(
-        {"CPUTime": "sum", "TotalCPU": "sum"}
+        {"CPUTime": "sum", "TotalCPU": "sum", "MaxRSS": "sum", "ReqMem": "sum"}
     )
     user_job_eff["cpu_eff"] = 100 * (user_job_eff.TotalCPU / user_job_eff.CPUTime)
+    user_job_eff["mem_eff"] = 100 * (user_job_eff.MaxRSS / user_job_eff.ReqMem)
 
     # Write to markdown
-    user_job_eff = user_job_eff[["cpu_eff"]]
+    user_job_eff = user_job_eff[["cpu_eff", "mem_eff"]]
+    user_job_eff.to_csv("user_job_eff.csv")
     user_job_eff.sort_values("cpu_eff", ascending=False, inplace=True)
-    user_job_eff.rename(columns={"cpu_eff": "CPU Eff. [%]"}, inplace=True)
+    user_job_eff.rename(
+        columns={"cpu_eff": "CPU Eff. [%]", "mem_eff": "Memory Eff. [%]"}, inplace=True
+    )
     with open("user_job_eff.md", "w") as io:
         user_job_eff.to_markdown(io, floatfmt=".1f", tablefmt="github")
